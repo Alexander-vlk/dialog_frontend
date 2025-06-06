acc1e8c2732682df99f7a05c63f9df670a5247d1
alex_barmen <www.lad282@gmail.com>
2025-06-06 13:06:21 +0300

    feat: decompose header blocks to different vue components

---
+++ src/components/common/header/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import ProfileHeader from "@/components/common/header/ProfileHeader.vue";
+import NavMenu from "@/components/common/header/NavMenu.vue";
+import Logo from "@/components/common/header/Logo.vue";
+
+document.getElementById('menu-toggle')?.addEventListener('click', function() {
+    document.getElementById('menu')?.classList.toggle('hidden');
+});
+</script>
+
+<template>
+<header class="bg-white shadow-md py-4 flex justify-center">
+    <div id="header-container" class="mx-auto w-full flex justify-between items-center px-6">
+        <Logo />
+        <NavMenu />
+        <ProfileHeader />
+    </div>
+</header>
+</template>
+
+<style scoped>
+#header-container {
+    max-width: 1200px;
+}
+</style>
---
+++ src/components/common/header/Logo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <a href="" class="text-primary text-2xl font-bold">
+        <span class="text-red-700">Dia</span><span class="text-blue-700">Log</span>
+    </a>
+</template>
+
+<style scoped>
+
+</style>
