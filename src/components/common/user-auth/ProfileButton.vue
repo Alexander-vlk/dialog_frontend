1b1ef356b144f6cccaef428d5592449b407808fe
alex_barmen <www.lad282@gmail.com>
2025-06-09 19:08:04 +0300

    feat: profile button

---
+++ src/components/common/user-auth/ProfileButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <a href="" class="bg-blue-500 text-white px-5 py-4 rounded-xl shadow-sm hover:bg-blue-700 transition">Профиль</a>
+</template>
+
+<style scoped>
+
+</style>
