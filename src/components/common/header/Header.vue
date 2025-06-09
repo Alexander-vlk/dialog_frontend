0fc1710352a400dd61a0bfb897e210ce0436a45a
alex_barmen <www.lad282@gmail.com>
2025-06-09 15:48:13 +0300

    feat: search field

--- src/components/common/header/Header.vue
+++ src/components/common/header/Header.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import NavMenu from "@/components/common/header/NavMenu.vue";
 import Logo from "@/components/common/header/Logo.vue";
+import SearchField from "@/components/common/header/SearchField.vue";
 
 document.getElementById('menu-toggle')?.addEventListener('click', function() {
     document.getElementById('menu')?.classList.toggle('hidden');
@@ -11,6 +12,7 @@ document.getElementById('menu-toggle')?.addEventListener('click', function() {
 <header class="bg-white shadow-md py-4 flex justify-center">
     <div class="mx-auto w-full flex justify-between items-center px-6">
         <Logo />
+        <SearchField />
         <NavMenu />
     </div>
 </header>
--- src/components/common/header/SearchField.vue
+++ src/components/common/header/SearchField.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+import { ref, onMounted, onBeforeUnmount } from "vue"
 
+const isActive = ref(false)
+const clickableBox = ref<HTMLElement | null>(null)
+const searchInputField = ref<HTMLInputElement | null>(null)
+
+const handleClickOutside = (e: MouseEvent) => {
+    if (!clickableBox.value?.contains(e.target)) {
+        isActive.value = false
+        return
+    }
+    searchInputField.value?.focus()
+}
+
+onMounted(() => {
+    document.addEventListener('click', handleClickOutside)
+})
+
+onBeforeUnmount(() => {
+    document.removeEventListener('click', handleClickOutside)
+})
 </script>
 
 <template>
-
+    <div
+        class="w-xl flex items-center gap-x-5 border py-3 px-4 rounded-full"
+        ref="clickableBox"
+        @click="handleClickOutside"
+        @click.stop="isActive = true"
+        :class="isActive ? 'border-blue-500' : 'border-gray-200'"
+    >
+        <MagnifyingGlassIcon class="w-6 h-6" />
+        <input type="text" id="searchInputField" placeholder="Поиск" class="w-full border-none outline-none h-full" ref="searchInputField" />
+    </div>
 </template>
 
 <style scoped>
