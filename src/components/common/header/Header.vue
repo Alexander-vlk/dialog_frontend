9ef0f31f9bdf2a21ba8d9b4ddd6addb0dd782fd9
alex_barmen <www.lad282@gmail.com>
2025-06-09 19:07:36 +0300

    refactor: add tab

--- src/components/common/header/Header.vue
+++ src/components/common/header/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import NavMenu from "@/components/common/header/nav-menu/NavMenu.vue";
+import NavMenu from "@/components/common/header/header-menu/HeaderMenu.vue";
 import Logo from "@/components/common/header/header-logo/Logo.vue";
 import HeaderCenter from "@/components/common/header/header-center/HeaderCenter.vue";
 
@@ -9,15 +9,15 @@ document.getElementById('menu-toggle')?.addEventListener('click', function() {
 </script>
 
 <template>
-<header class="bg-white shadow-md py-4 flex justify-center">
-    <div class="mx-auto w-full flex justify-between items-center px-6">
-        <Logo />
-        <div class="w-2xl hidden md:flex items-center gap-x-5">
-            <HeaderCenter />
+    <header class="bg-white shadow-md py-4 flex justify-center">
+        <div class="mx-auto w-full flex justify-between items-center px-6">
+            <Logo />
+            <div class="w-2xl hidden md:flex items-center gap-x-5">
+                <HeaderCenter />
+            </div>
+            <NavMenu />
         </div>
-        <NavMenu />
-    </div>
-</header>
+    </header>
 </template>
 
 <style scoped>
