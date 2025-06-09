63937517a11edf3851214fa3f35a8d133a84e6ee
alex_barmen <www.lad282@gmail.com>
2025-06-09 19:07:55 +0300

    feat: header menu links

---
+++ src/components/common/header/header-menu/HeaderMenuLinks.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<nav>
+    <ul>
+        <li>
+            <a href="" class="text-xl">Главная</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">Личный кабинет</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">Форум</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">Семейный доступ</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">О нас</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">Отзывы</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">База знаний</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">Политика конфиденциальности</a>
+        </li>
+        <li>
+            <a href="" class="text-xl">FAQ</a>
+        </li>
+    </ul>
+</nav>
+</template>
+
+<style scoped>
+
+</style>
