818a699971b54ab969dd2ab1a3174008d2cdff96
alex_barmen <www.lad282@gmail.com>
2025-06-13 20:09:00 +0300

    feat: swiper block component

--- src/views/main-page/HomeView.vue
+++ src/views/main-page/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HeroActionBlock from "@/components/main-page/HeroActionBlock.vue";
+import SwiperBlock from "@/components/main-page/SwiperBlock.vue";
 
 document.title = 'Главная'
 </script>
 
 <template>
-    <section class="mx-auto px-6 py-12 mt-16">
+    <section class="mx-auto px-6 py-12">
+        <SwiperBlock />
         <HeroActionBlock />
     </section>
 </template>
