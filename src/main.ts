dff790cfb66a492af49282006a4ddd2bed8ac968
alex_barmen <www.lad282@gmail.com>
2025-06-13 11:24:31 +0300

    feat: remove const from main ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
 import router from "@/router";
 
-const app = createApp(App)
+import App from './App.vue'
 
-const apiUrl = import.meta.env.VITE_BACKEND_API_URL
+const app = createApp(App)
 
 app.use(createPinia())
 
