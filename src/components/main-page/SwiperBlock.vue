a19648dcdcd050e17eeb714a59bc5d4ea91085ca
alex_barmen <www.lad282@gmail.com>
2025-06-13 20:10:17 +0300

    refactor: swiper block component

--- src/components/main-page/SwiperBlock.vue
+++ src/components/main-page/SwiperBlock.vue
@@ -8,7 +8,6 @@ import api from '@/plugins/axios/axios.ts'
 import { backendUrl } from '@/constants/common.ts'
 import type {Slide} from "@/types/mainPageTypes.ts";
 
-console.log(backendUrl)
 const sliderImages = ref<Array<Slide>>([])
 
 onMounted(async () => {
@@ -24,26 +23,26 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="w-full max-w-7xl mx-auto py-10">
-    <Swiper
-      :slides-per-view="1"
-      :space-between="20"
-      :pagination="{ clickable: true }"
-      :autoplay="{ delay: 5000 }"
-      loop
-      class="rounded-2xl shadow-lg"
-    >
-      <SwiperSlide
-        v-for="(slideImage, index) in sliderImages"
-        :key="index"
-        class="bg-blue-50 flex flex-col items-start gap-4"
-      >
-          <img :src="backendUrl + slideImage.image_url" :alt="slideImage.alt">
-      </SwiperSlide>
+    <div class="w-full max-w-7xl mx-auto py-10">
+        <Swiper
+            :slides-per-view="1"
+            :space-between="20"
+            :pagination="{ clickable: true }"
+            :autoplay="{ delay: 5000 }"
+            loop
+            class="rounded-2xl shadow-lg"
+        >
+            <SwiperSlide
+                v-for="(slideImage, index) in sliderImages"
+                :key="index"
+                class="bg-blue-50 flex flex-col items-start gap-4"
+            >
+                <img :src="backendUrl + slideImage.image_url" :alt="slideImage.alt">
+            </SwiperSlide>
 
-      <div class="swiper-pagination mt-4"></div>
-    </Swiper>
-  </div>
+            <div class="swiper-pagination mt-4"></div>
+        </Swiper>
+    </div>
 </template>
 
 <style scoped>
