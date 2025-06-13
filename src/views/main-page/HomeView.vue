dbc2037ed65909bff7ac7c1670aed17a37804e6c
alex_barmen <www.lad282@gmail.com>
2025-06-13 11:24:47 +0300

    feat: show hero action block on main page

--- src/views/main-page/HomeView.vue
+++ src/views/main-page/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import HeroActionBlock from "@/components/main-page/HeroActionBlock.vue";
 
+document.title = 'Главная'
 </script>
 
 <template>
-
+    <section class="mx-auto px-6 py-12 mt-16">
+        <HeroActionBlock />
+    </section>
 </template>
 
 <style scoped>
