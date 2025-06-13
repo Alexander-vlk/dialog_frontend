058860c27f181d5bb25389b5cc3f3198c42b88fa
alex_barmen <www.lad282@gmail.com>
2025-06-13 11:24:20 +0300

    feat: hero action block component

---
+++ src/components/main-page/HeroActionBlock.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import api from '@/plugins/axios/axios.ts'
+import {onMounted, reactive} from "vue";
+
+const heroActionBlock = reactive({
+    slogan: '',
+    short_description: '',
+    button_text: '',
+})
+
+onMounted(async () => {
+    try {
+        const response = await api.get('sitesettings/hero-action-block')
+        Object.assign(heroActionBlock, response.data)
+    }
+    catch (error) {
+        console.error('Ошибка загрузки данных: ', error)
+    }
+})
+</script>
+
+<template>
+    <section
+        v-if="heroActionBlock.slogan"
+        class="text-center max-w-3xl mx-auto"
+    >
+        <h1 class="text-4xl font-bold text-gray-900">{{ heroActionBlock.slogan }}</h1>
+        <p class="text-gray-600 mt-4 text-lg">{{ heroActionBlock.short_description }}</p>
+        <div class="mt-6">
+            <a href="" class="bg-blue-500 text-white px-6 py-3 rounded-lg text-lg hover:bg-green-700 shadow-md transition">
+                {{ heroActionBlock.button_text }}
+            </a>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
