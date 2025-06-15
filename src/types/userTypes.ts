ceabefd76d59d56d9778aa86df43bd7383678bba
alex_barmen <www.lad282@gmail.com>
2025-06-15 14:03:30 +0300

    feat: user types

---
+++ src/types/userTypes.ts
@@ -0,0 +1,17 @@
+export type User = {
+    id: number,
+    username: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+}
+export type UserProfile = {
+    patronymicName: string,
+    phoneNumber: string,
+    imageUrl: string,
+    gender: string,
+    birthDate: string,
+    diagnosisDate: string,
+    diabetesType: string,
+    treatmentType: string,
+}
