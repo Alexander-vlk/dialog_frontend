9e07db65b0b7c1362f154288ff0364c09eb2e38e
alex_barmen <www.lad282@gmail.com>
2025-08-09 23:36:17 +0300

    feat: set user data in store

---
+++ src/utils/common/fetchUserData.ts
@@ -0,0 +1,22 @@
+import api from '@/plugins/axios/axios.ts'
+import { userAuthStore } from '@/stores/user.ts'
+import type { User } from '@/types/userTypes.ts'
+
+
+export default async function fetchUserData(): Promise<User | undefined> {
+    const userStore = userAuthStore()
+    try {
+        const response = await api.get(
+            'cabinet/private/api/users/',
+            {
+                headers: {
+                    Authorization: `Bearer ${userStore.accessToken}`,
+                }
+            }
+        )
+        return response.data
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
