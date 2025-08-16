331701b6bdbe684c3cc6aa1bdf1f15a4020e3bb2
alex_barmen <www.lad282@gmail.com>
2025-08-16 20:19:14 +0300

    feat: error screen and preloader shows until all requests are not executed

--- src/components/ui/PageLoader.vue
+++ src/components/ui/PageLoader.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
+import api from '@/services/axios.ts'
+import Logo from '@/components/common/header/header-logo/Logo.vue'
 
 const isLoading = ref(true)
-onMounted(() => {
-    window.addEventListener('load',
-        async () => {
-            isLoading.value = false
+const isErrorOccurred = ref(false)
+
+let activeRequests = 0
+api.interceptors.request.use((config) => {
+    activeRequests++;
+    isLoading.value = true;
+    return config;
+});
+api.interceptors.response.use(
+    (response) => {
+        activeRequests--;
+        if (activeRequests === 0) {
+            isLoading.value = false;
         }
-    )
-})
+        return response;
+    },
+    (error) => {
+        activeRequests--;
+        if (activeRequests === 0) {
+            isLoading.value = false;
+            isErrorOccurred.value = true;
+        }
+        return Promise.reject(error);
+    }
+);
+
+if (!activeRequests) {
+    isLoading.value = false;
+}
 </script>
 
 <template>
@@ -17,6 +41,14 @@ onMounted(() => {
         class="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-100"
     >
         <div class="w-16 h-16 border-4 border-blue-700 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+    <div
+        v-if="isErrorOccurred"
+        class="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-100"
+    >
+        <p class="text-3xl">
+            Что-то пошло не так...
+        </p>
     </div>
 </template>
 
