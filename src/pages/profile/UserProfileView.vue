aac19a0fd56ab406d70305721b9ef85e755188f4
alex_barmen <www.lad282@gmail.com>
2025-08-24 19:23:29 +0300

    feat: profile template

--- src/pages/profile/UserProfileView.vue
+++ src/pages/profile/UserProfileView.vue
@@ -12,7 +12,45 @@ document.title = "Личный кабиет";
 </script>
 
 <template>
-    <h2>Личный кабинет</h2>
+    <div
+        class="md:grid grid-cols-3 md:gap-3 m-5"
+    >
+        <div
+            class="bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[30lvh]"
+        >
+            <h2>Данные профиля</h2>
+        </div>
+        <div
+            class="md:col-span-2 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[30lvh]"
+        >
+            <h2>Сводка</h2>
+        </div>
+        <div
+            class="md:col-span-2 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[40lvh]"
+        >
+            <h2>Дневной отчет</h2>
+        </div>
+        <div
+            class="md:col-span-1 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[40lvh]"
+        >
+            <h2>Ссылки</h2>
+        </div>
+        <div
+            class="md:col-span-3 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[40lvh]"
+        >
+            <h2>Недельный отчет</h2>
+        </div>
+        <div
+            class="md:col-span-2 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[10lvh]"
+        >
+            <h2>Еще ссыылки куда-нибудь там</h2>
+        </div>
+        <div
+            class="md:col-span-1 bg-white rounded-xl shadow p-4 my-2 md:my-0 min-h-[10lvh]"
+        >
+            <h2>Блок призыва к действию или чето такое</h2>
+        </div>
+    </div>
 </template>
 
 <style scoped>
