00387f491c1c29b28f6e872169ebd409322ef171
alex_barmen <www.lad282@gmail.com>
2025-09-09 14:33:43 +0300

    feat: user logo and main info

--- src/components/profile/ProfileData.vue
+++ src/components/profile/ProfileData.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { userAuthStore } from '@/stores/user.ts'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 
+const { user } = storeToRefs(userAuthStore())
+const imageUrl = ref(`https://${window.location.hostname}${user.value.image_url}`)
 </script>
 
 <template>
-    <h2>Данные профиля</h2>
+    <div class="flex items-center gap-4">
+        <img
+            :src="imageUrl"
+            :alt="user?.username"
+            class="w-16 h-16 rounded-full object-cover border"
+        />
+        <div class="min-w-0">
+            <div class="text-base font-semibold text-gray-900">@{{ user.username }}</div>
+            <div class="text-sm text-gray-600 truncate">
+                {{ user.last_name }} {{ user.first_name }} {{ user?.patronymic_name }}
+            </div>
+        </div>
+    </div>
+    <div>
+
+    </div>
 </template>
 
 <style scoped>
