d5c381f5095ba2404cb38b5b5005b85f7e5a7b2f
alex_barmen <www.lad282@gmail.com>
2025-09-10 11:58:45 +0300

    feat: todo component

---
+++ src/components/dev/ToDo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineProps({
+    todoText: String
+})
+</script>
+
+<template>
+    <span
+        class="text-orange-500 font-bold italic cursor-pointer"
+        :title="todoText"
+    >
+        TODO
+    </span>
+</template>
+
+<style scoped>
+
+</style>
