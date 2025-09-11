f4016a03ea9bd3866b3518a80afb4f392b088f04
alex_barmen <www.lad282@gmail.com>
2025-09-11 18:13:09 +0300

    feat: other apps links

--- src/components/profile/AnotherAppsLinks.vue
+++ src/components/profile/AnotherAppsLinks.vue
@@ -3,8 +3,20 @@
 </script>
 
 <template>
-    <h2>Ссылки на другие приложения</h2>
-
+    <div
+        class="flex flex-wrap sm:flex-nowrap gap-2 items-center justify-center"
+    >
+        <div
+            class="w-full bg-blue-500 px-2 py-2 sm:py-4 text-center rounded-xl shadow text-white hover:bg-blue-700 transition cursor-pointer"
+        >
+            <RouterLink :to="{name: 'main'}">Форум</RouterLink>
+        </div>
+        <div
+            class="w-full bg-blue-500 px-2 py-2 sm:py-4 text-center rounded-xl shadow text-white hover:bg-blue-700 transition cursor-pointer"
+        >
+            <RouterLink :to="{name: 'main'}">Новости</RouterLink>
+        </div>
+    </div>
 </template>
 
 <style scoped>
