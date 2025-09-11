668556ab77509cafc6655ff74f5bf3e6e8f2b27d
alex_barmen <www.lad282@gmail.com>
2025-09-11 12:06:32 +0300

    feat: buttons for modal forms

--- src/components/profile/ModalForms.vue
+++ src/components/profile/ModalForms.vue
@@ -3,7 +3,48 @@
 </script>
 
 <template>
-    <h2>Блок модальных окон</h2>
+    <h2
+        class="text-xl font-bold"
+    >
+        Внести показатели
+    </h2>
+    <div
+        class="my-2"
+    >
+        <button
+            class="bg-blue-500 text-white p-2 w-full rounded-xl shadow cursor-pointer hover:bg-blue-700 transition"
+        >
+            Хлебные единицы
+        </button>
+    </div>
+    <div
+        class="my-2"
+    >
+        <button
+            class="bg-blue-500 text-white p-2 w-full rounded-xl shadow cursor-pointer hover:bg-blue-700 transition"
+        >
+            Давление
+        </button>
+    </div>
+    <div
+        class="my-2"
+    >
+        <button
+            class="bg-blue-500 text-white p-2 w-full rounded-xl shadow cursor-pointer hover:bg-blue-700 transition"
+        >
+            Сахар
+        </button>
+    </div>
+    <div
+        class="my-2"
+    >
+        <button
+            class="bg-blue-500 text-white p-2 w-full rounded-xl shadow cursor-pointer hover:bg-blue-700 transition"
+        >
+            Температура
+        </button>
+    </div>
+    <p>Вносить показатели можно не чаще одного раза в 15 минут</p>
 </template>
 
 <style scoped>
