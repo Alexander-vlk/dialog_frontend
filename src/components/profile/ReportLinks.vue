35023a2f8ba4166a99a491ec75c08ab5161b43d2
alex_barmen <www.lad282@gmail.com>
2025-09-11 17:31:23 +0300

    feat: all reports links

--- src/components/profile/ReportLinks.vue
+++ src/components/profile/ReportLinks.vue
@@ -3,7 +3,26 @@
 </script>
 
 <template>
-    <h2>Ссылки</h2>
+    <h2
+        class="text-xl font-bold"
+    >
+        Все отчеты
+    </h2>
+    <div
+        class="w-full bg-blue-500 p-2 text-center rounded-xl shadow my-2 text-white hover:bg-blue-700 transition cursor-pointer"
+    >
+        <RouterLink :to="{name: 'main'}">Дневные</RouterLink>
+    </div>
+    <div
+        class="w-full bg-blue-500 p-2 text-center rounded-xl shadow my-2 text-white hover:bg-blue-700 transition cursor-pointer"
+    >
+        <RouterLink :to="{name: 'main'}">Недельные</RouterLink>
+    </div>
+    <div
+        class="w-full bg-blue-500 p-2 text-center rounded-xl shadow my-2 text-white hover:bg-blue-700 transition cursor-pointer"
+    >
+        <RouterLink :to="{name: 'main'}">Месячные</RouterLink>
+    </div>
 </template>
 
 <style scoped>
