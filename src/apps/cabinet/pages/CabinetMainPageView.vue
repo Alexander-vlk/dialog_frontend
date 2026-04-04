2cd0f92ab0357633ac20c3ca252384eda92484f6
alex_barmen <www.lad282@gmail.com>
2026-04-04 20:51:00 +0300

    feat: rename components

---
+++ src/apps/cabinet/pages/CabinetMainPageView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import CabinetMainPageComponent from "@/apps/cabinet/components/CabinetMainPageComponent.vue";
+</script>
+
+<template>
+    <CabinetMainPageComponent />
+</template>
+
+<style scoped>
+
+</style>
